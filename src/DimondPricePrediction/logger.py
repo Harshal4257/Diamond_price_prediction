72a5285421a1d76b197229015baeee69dec7c08c
harshal4257 <harshalpatil8482@gmail.com>
2025-01-06 11:38:59 +0530

    logging file updated

---
+++ src/DimondPricePrediction/logger.py
@@ -0,0 +1,17 @@
+import os 
+import logging
+from datetime import datetime
+
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_path = os.path.join("notebooks","logs")
+
+os.makedirs(log_path,exist_ok=True)
+
+log_filepath = os.path.join(log_path,log_file)
+
+logging.basicConfig(level=logging.INFO,
+                    filename=log_filepath,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+                    
+)
