439b4268cd7fc026eb2627587bd71946e0e341a5
harshal4257 <harshalpatil8482@gmail.com>
2025-01-17 12:30:41 +0530

    prediction pipeline updated

--- src/DimondPricePrediction/utils/utils.py
+++ src/DimondPricePrediction/utils/utils.py
@@ -9,6 +9,7 @@ from src.Dimondpriceprediction.exception import customexception
 from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
 
 def save_object(file_path, obj):
+    
     try:
         dir_path = os.path.dirname(file_path)
 
@@ -21,20 +22,16 @@ def save_object(file_path, obj):
         raise customexception(e, sys)
     
 def evaluate_model(X_train,y_train,X_test,y_test,models):
+    
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
-            # Train model
-            model.fit(X_train,y_train)
-
             
-
-            # Predict Testing data
+            model.fit(X_train,y_train)
+            
             y_test_pred =model.predict(X_test)
 
-            # Get R2 scores for train and test data
-            #train_model_score = r2_score(ytrain,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]] =  test_model_score
@@ -43,4 +40,14 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         logging.info('Exception occured during model training')
+        raise customexception(e,sys)
+    
+def load_object(file_path):
+    
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        logging.info("Error occured at load_object in utils")
         raise customexception(e,sys)
