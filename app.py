f5dbf9dbbac8c6d5e32e191ec115ea10c0609ade
harshal4257 <harshalpatil8482@gmail.com>
2025-02-02 12:27:21 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
 from src.Dimondpriceprediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
 from flask import Flask,render_template,request,jsonify
 
